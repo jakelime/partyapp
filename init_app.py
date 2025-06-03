3a9c3465c461b824412363faeb0c44a44bfd0ae4
Jake Lim <lim.jiahao@stengg.com>
2025-06-03 13:42:12 +0800

    use initapp to initialize employee db

--- init_app.py
+++ init_app.py
@@ -1,6 +1,43 @@
+import subprocess
+
 from main.main.utils import DjangoVersionManager
 
-dvm = DjangoVersionManager()
-dvm.write_app_version_file()
 
-print(f"{dvm.get_app_version()=}")
+def run_command(cmds: list[str]) -> list:
+    try:
+        print(f"running commands: {' '.join(cmds)}")
+        cp = subprocess.run(
+            cmds,
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        print(cp.stdout)
+        if cp.stderr:
+            print(cp.stderr)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running Django commands: {e.stderr}")
+
+
+def run_django_commands():
+    run_command(["python", "main/manage.py", "makemigrations"])
+    run_command(["python", "main/manage.py", "migrate"])
+    run_command(["python", "main/manage.py", "import_employees"])
+    print("All Django commands executed successfully.")
+
+
+def init_version():
+    dvm = DjangoVersionManager()
+    dvm.write_app_version_file()
+    version = dvm.get_app_version()
+    print(f"App version initialized: {version}")
+    return version
+
+
+def main():
+    init_version()
+    run_django_commands()
+
+
+if __name__ == "__main__":
+    main()
